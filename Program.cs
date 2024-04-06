e9a47f64a45a2e40057c6397a64ef8ffd0684123
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-06 19:35:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("192.168.100.3", 8888);
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = new byte[1024];
+            int bytesRead = stream.Read(data, 0, data.Length);
+
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
